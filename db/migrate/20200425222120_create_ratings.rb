a8fec37f0563cdc27e2e09cb2371d886ff8d1844
bavarianrhino <rjriesenberger@gmail.com>
2020-04-25 18:28:41 -0400

    moved rater migrate data

--- db/migrate/20200425222120_create_ratings.rb
+++ db/migrate/20200425222120_create_ratings.rb
@@ -1,12 +1,12 @@
 class CreateRatings < ActiveRecord::Migration[6.0]
   def change
     create_table :ratings do |t|
-      t.decimal :rating
-      t.references :rater, null: false
-      t.references :user, null: false, foreign_key: true
-      t.datetime :rated_at
+        t.references :user, null: false, foreign_key: true
+        t.references :rater, null: false
+        t.integer :rating
+        t.datetime :rated_at
 
-      t.timestamps
+        t.timestamps
     end
   end
 end
