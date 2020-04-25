b866dc149ffce4e4f3478e3c6aec531de7db3751
bavarianrhino <rjriesenberger@gmail.com>
2020-04-25 17:12:01 -0400

    added all routes for future models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # Created namespaces for integration of future api versions.
+    # Also, for early stage development I open up all routes for debugging purposes.
+    namespace :api do
+        namespace :v1 do
+            # resources :users, only: [:index, :create, update, destroy]
+            resources :users
+            resources :posts
+            resources :comments
+            resources :ratings
+            resources :comments
+        end
+    end
 end
