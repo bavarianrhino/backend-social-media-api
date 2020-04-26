20032d7c54e5a2cbe396776909053c2e270d16b3
bavarianrhino <rjriesenberger@gmail.com>
2020-04-25 22:46:23 -0400

    added posts csv seed parser

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,24 @@ CSV.foreach(Rails.root.join('db/csv/users.csv'), headers: true) do |row|
         email: row[1],
         name: row[2],
         github_username: row[3], 
-        registered_at: row[4]
+        registered_at: row[4],
+        created_at: row[5],
+        updated_at: row[6]
     })
     puts User
+end
+
+# Import Posts
+CSV.foreach(Rails.root.join('db/csv/posts.csv'), headers: true) do |row|
+  
+    Post.create({
+        id: row[0],
+        title: row[1],
+        body: row[2],
+        user_id: row[3], 
+        posted_at: row[4],
+        created_at: row[5],
+        updated_at: row[6]
+    })
+    puts Post
 end
