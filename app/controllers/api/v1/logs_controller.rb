570bf9bf0e2cf40acbf1e4dac20da1ab7ecf09a9
bavarianrhino <rjriesenberger@gmail.com>
2020-04-26 19:54:05 -0400

    added boiler plate logs controller

---
+++ app/controllers/api/v1/logs_controller.rb
@@ -0,0 +1,49 @@
+class API::V1::LogsController < ApplicationController
+
+    def index
+        @logs = Log.all
+        # Create model json helpers
+        # render json: @logs.all_json
+        render json: @logs
+    end
+
+    def show
+        @log = Log.find(params[:id])
+        render json: @log
+    end
+
+    def create
+        @log = Log.new(log_params)
+        if @log && @log.valid?
+            @log.save
+            render json: { log: @log }, status: :created
+        else
+            render json: { errors: @log.errors.full_messages }, status: :not_acceptable
+        end
+    end
+  
+    def update
+        @log = Log.find(params[:id])
+        if @log.update(log_params)
+            render json: { log: @log }, status: :accepted
+        else
+            render json: { errors: @log.errors.full_messages }, status: :not_acceptable
+        end
+    end
+
+    def destroy
+        @log = Log.find(params[:id])
+        if @log
+            @log.destroy
+            render json: {}, status: :no_content
+        else
+            render json: { errors: @log.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+    
+    private
+    def log_params
+        
+    end
+
+end
