87bdddfe4102e6ecf8586d9cfe72cb04a8585b44
bavarianrhino <rjriesenberger@gmail.com>
2020-04-27 04:42:33 -0400

    logs for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :post, dependent: :destroy
     has_one :log, as: :loggable, dependent: :destroy
+
+    after_save :create_log
+
+    def create_log
+        log = Log.new
+        log.description = "New Comment -  #{self.message}"
+        log.type_of = "Comment"
+        log.user_id = self.user_id
+        log.loggable_id = self.id
+        log.loggable_type = self.class.name
+        log.posted_at = self.commented_at
+        log.save
+    end
 end
