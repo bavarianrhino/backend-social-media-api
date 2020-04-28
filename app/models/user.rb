0829b51a98750fc7cda752a1a7c4da694bfda1a0
bavarianrhino <rjriesenberger@gmail.com>
2020-04-27 21:06:49 -0400

    full functioning db and assosiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,9 @@ class User < ApplicationRecord
             github_username: self.github_username,
             registered_at: self.registered_at,
             rating: self.user_average_rating,
-            github: self.create_github_logs
+            github: self.create_github_logs,
             logs: self.logs.all_json,
-            ratings: self.ratings.all_json,
+            ratings: self.ratings.all_json
         }
     end
 
@@ -35,27 +35,26 @@ class User < ApplicationRecord
 
     def create_github_logs
         if (self.github_username != nil)
+            puts self.github_username
             base_url = "https://api.github.com/users/#{self.github_username}/events"
             begin
                 response = RestClient.get(base_url)
             rescue RestClient::ExceptionWithResponse => err
                 []
             else
-                response = JSON.parse(response).select do |gitlog|
-                    if (["PushEvent", "CreateEvent"].include? (gitlog["type"]))
-                        log = Log.find_by(github_event_id: gitlog['id']) || Log.new
-                        log.type_of = gitlog['type']
-                        log.description = "Pushed/Created #{gitlog['type']} was made for #{gitlog['repo']['name']} with #{gitlog['payload']['size']} commit(s) to #{gitlog['repo']['name']}."
-                        log.github_event_id = gitlog['id']
-                        log.loggable_id = self.id
-                        log.loggable_type = 'Github Log'
-                        log.posted_at = gitlog['created_at']
-                        log.user_id = self.id
-                        log.save
-                        puts log.values
-                    else
-                        puts gitlog.values
-                    end
+                parsedresponse = JSON.parse(response).select { |e| (["PushEvent", "CreateEvent"].include? (e["type"])) }
+                parsedresponse.each do |gitlog|
+                    puts gitlog
+                    log = Log.find_by(github_event_id: gitlog['id']) || Log.new
+                    log.type_of = gitlog['type']
+                    log.description = "Pushed/Created #{gitlog['type']} was made for #{gitlog['repo']['name']} with #{gitlog['payload']['size']} commit(s) to #{gitlog['repo']['name']}."
+                    log.github_event_id = gitlog['id']
+                    log.loggable_id = self.id
+                    log.loggable_type = 'Github Log'
+                    log.posted_at = gitlog['created_at']
+                    log.user_id = self.id
+                    log.save
+                    puts log
                 end
             end
         end
